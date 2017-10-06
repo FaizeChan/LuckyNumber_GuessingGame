2be5c40091cf2897bef1f0e2b0deb412b49d5f5b
FaizeChan <obey.faize@gmail.com>
2017-10-06 10:19:43 -0400

    first commit

---
+++ LuckyNumber_GuessingGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuckyNumber_GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
