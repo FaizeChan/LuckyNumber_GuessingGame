0edc67ec6e06801bd6fe3e1723cbf8aa193c4841
FaizeChan <obey.faize@gmail.com>
2017-10-06 14:50:36 -0400

    third commit

--- LuckyNumber_GuessingGame/Program.cs
+++ LuckyNumber_GuessingGame/Program.cs
@@ -11,7 +11,7 @@ namespace LuckyNumber_GuessingGame
         static void Main(string[] args)
         {
 
-           
+
 
             Console.WriteLine("\n");
             Console.WriteLine("Hello, and Welcome to Faizon's Marvelous Cash Making Casino Playing \nAward Winning Extravaganza!");
@@ -37,11 +37,35 @@ namespace LuckyNumber_GuessingGame
                 Console.WriteLine("That's not one of the options, so I will take that as a no. Maybe next time.");
                 Environment.Exit(0);
             }
+            Console.WriteLine("\n");
             Console.WriteLine("Alright, what is the ending number of the highest number in the number range?");
             int endingNumber = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Cool. Now guess (6) numbers you think will be the lucky numbers generated within the number range.");
+            Console.WriteLine("\n");
+            Console.WriteLine("Cool. Now guess (6) numbers you think will be the lucky numbers \ngenerated within the number range.");
 
+                int[] index = new int[6];
+                for (int i = 0; i < index.Length; i++)
+                {
+                    index[i] = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Good, guess another number.");
+                }
+                
+                Console.WriteLine("\n");
+                Random luckyNumbers = new Random();
+                for (int luckyNumber = 1; luckyNumber <= 6; luckyNumber++)
+           {
+                int guessedNumber = luckyNumbers.Next(endingNumber) + 1;
+                Console.Write("\nLucky Number ");
+                Console.WriteLine("{0}", guessedNumber);
+
+                if (guessedNumber > endingNumber)
+                {
+                    Console.WriteLine("Please give a valid number.");
+                }
+            }
+
+                       
         }
     }
 }
