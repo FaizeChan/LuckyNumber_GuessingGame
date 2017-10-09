2fa8196c888217e7664cd718038d083d9d1a93e5
FaizeChan <obey.faize@gmail.com>
2017-10-08 23:19:29 -0400

    almost done but bot there yet

--- LuckyNumber_GuessingGame/Program.cs
+++ LuckyNumber_GuessingGame/Program.cs
@@ -10,37 +10,66 @@ namespace LuckyNumber_GuessingGame
     {
         static void Main(string[] args)
         {
-            //Simple ask the smallest below above | and give the smallest integer 
 
-            Console.WriteLine("Alright, Lets Begin. What is the starting number for the lowest number in the \nnumber range?");
-            int startingNumber = int.Parse(Console.ReadLine());
 
-            //Ask the largest number below | and give largest integer
 
-            Console.WriteLine("\n");
-            Console.WriteLine("Alright, what is the ending number of the highest number in the number range?");
-            int endingNumber = int.Parse(Console.ReadLine());
+            string playAgain = "";
 
-            //Now here comes the first Array and loop with an if statement.
-
-            Console.WriteLine("\n");
-            Console.WriteLine("Cool. Now guess (6) numbers you think will be the lucky numbers \ngenerated within the number range.");
-            int[] usrGuess = new int[6];
-            for (int i = 0; i < usrGuess.Length; i++)
+            do
             {
-                usrGuess[i] = int.Parse(Console.ReadLine());
+
+                //Simple ask the smallest below above | and give the smallest integer 
+
+                Console.WriteLine("Alright, Lets Begin. What is the starting number for the lowest number in the \nnumber range?");
+                int startingNumber = int.Parse(Console.ReadLine());
+
+                //Ask the largest number below | and give largest integer
+
+                Console.WriteLine("\n");
+                Console.WriteLine("Alright, what is the ending number of the highest number in the number range?");
+                int endingNumber = int.Parse(Console.ReadLine());
+
+                //Now here comes the first Array and loop with an if statement.
+
+                Console.WriteLine("\n");
+                Console.WriteLine("Cool. Now guess (6) numbers you think will be the lucky numbers \ngenerated within the number range.");
+                int[] usrGuess = new int[6];
+                for (int i = 0; i < usrGuess.Length; i++)
                 {
-                    if (usrGuess[i] < startingNumber || usrGuess[i] > endingNumber)
+                    usrGuess[i] = int.Parse(Console.ReadLine());
                     {
-                        Console.WriteLine("Please enter a valid number");
+                        while (usrGuess[i] < startingNumber || usrGuess[i] > endingNumber)
+                        {
+                            Console.WriteLine("Please enter a valid number.");
+                            usrGuess[i] = int.Parse(Console.ReadLine());
+                        }
                     }
                 }
+                // Just hardcoding the reward for part 3 here.
+                int Jackpot = 250000;
+
+                    //Going to have the computer radomize and pick a number between the starting and ending number.
+
+                    int[] pcNumber = new int[6];
+                    Random rand = new Random();
+                    for (int j = 0; j < pcNumber.Length; j++)
+                    {
+                        int randomNumber = rand.Next(startingNumber, endingNumber) + 1;
+                        pcNumber[j] = randomNumber;
+                        Console.WriteLine("Lucky Number: " + pcNumber[j]);
                 }
+                
 
 
 
 
-             
+
+
+
+
+                Console.WriteLine("Do you want to play again? (Yes/No)");
+                playAgain = Console.ReadLine().ToLower();
+            } while (playAgain == "yes");
         }
     }
 }
